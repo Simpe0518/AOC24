65df1c91dd84cc9b13cc4ce96456909974fec3aa
siman492 <siman492@student.liu.se>
2024-12-07 12:51:00 +0100

    cleanup

--- d7.py
+++ d7.py
@@ -1,17 +1,20 @@
 file = open("d7.txt", "r")
 inst = []
-tot = 0
+tot_p1 = 0
+tot_p2 = 0
 
 for line in file.readlines():
     inst.append(list(map(int, line.replace(':', '').split())))
 
-def valid(n, seq):
+def valid(n, seq, conc):
     if len(seq) == 1:
         return 1 if n == seq[0] else 0
-    return n if valid(n,[seq[0]+seq[1]]+seq[2:]) + valid(n,[seq[0]*seq[1]]+seq[2:]) + valid(n,[int(str(seq[0])+str(seq[1]))]+seq[2:])> 0 else 0
+    return n if valid(n,[seq[0]+seq[1]]+seq[2:],conc) + valid(n,[seq[0]*seq[1]]+seq[2:],conc) + (valid(n,[int(str(seq[0])+str(seq[1]))]+seq[2:],conc) if conc else 0)  > 0 else 0
 
 ### part 1 & 2 ###
 for seq in inst:
-    tot = tot + valid(seq[0], seq[1:])
+    tot_p1 = tot_p1 + valid(seq[0], seq[1:], False)
+    tot_p2 = tot_p2 + valid(seq[0], seq[1:], True)
 
-print(tot)
+print(tot_p1)
+print(tot_p2)
